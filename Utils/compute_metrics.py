e74e9e71f648e82025f95b6f13b691ebcaaf1448
Luca Geminiani <145434726+LucaGeminiani00@users.noreply.github.com>
2024-11-24 18:28:18 +0100

    Update compute_metrics.py

--- Utils/compute_metrics.py
+++ Utils/compute_metrics.py
@@ -5,14 +5,12 @@ from Utils.predictive_score import predictive_score
 
 def compute_metrics(ori_data, fake_data): 
     FID_score = Context_FID(ori_data, fake_data)
-    discriminative, fake_accuracy, real_accuracy = discriminative_score(ori_data, fake_data)
+    discriminative = discriminative_score(ori_data, fake_data)
     predictive_score_value = predictive_score(ori_data, fake_data)
     
     metrics = {
         "FID_score": FID_score,
         "discriminative_score": discriminative,
-        "fake_data_accuracy": fake_accuracy,
-        "real_data_accuracy": real_accuracy,
         "predictive_score": predictive_score_value
     }
     for key, value in metrics.items():
